9243a96e49d561a3ad9647133266d0ebc09e56d4
Viache-Slav <slavikfedko@gmail.com>
2023-11-19 19:44:53 +0100

    start_1
    wiele błędów, ale mam już spaczoną głowę.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-export const App = () => {
+// src/components/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import MovieDetails from './MovieDetails/MovieDetails';
+import Cast from './MovieDetails/Cast';
+import Reviews from './MovieDetails/Reviews';
+import MovieList from './Home/MovieList';
+
+const Header = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/movies">Movies</Link></li>
+      </ul>
+    </nav>
   );
 };
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<MovieList />} />
+        <Route path="/movies" element={<MovieList />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />} />
+        <Route path="/movies/:movieId/cast" element={<Cast />} />
+        <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
